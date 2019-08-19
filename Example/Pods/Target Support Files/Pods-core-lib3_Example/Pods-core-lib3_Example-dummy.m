bd4c13a4b1825d65a5065bd11a0d4c8a64d46032
yaoxin <yaoxin@sunlands.com>
2019-08-19 15:20:19 +0800

    add

---
+++ Example/Pods/Target Support Files/Pods-core-lib3_Example/Pods-core-lib3_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_core_lib3_Example : NSObject
+@end
+@implementation PodsDummy_Pods_core_lib3_Example
+@end
---
+++ Example/Pods/Target Support Files/core-lib3/core-lib3-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_core_lib3 : NSObject
+@end
+@implementation PodsDummy_core_lib3
+@end
